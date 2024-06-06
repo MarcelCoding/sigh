67e758061a80a1ec55799ced8beaf9939aae5bb1
Marcel <me@m4rc3l.de>
2024-06-06 23:50:38 +0200

    added rsa siging using rsa crate

--- src/alg/mod.rs
+++ src/alg/mod.rs
@@ -1,16 +1,16 @@
 use crate::{Error, PrivateKey, PublicKey};
 mod rsa_sha256;
-mod hs2019;
-
-use openssl::{hash::MessageDigest, sign::{Signer, Verifier}, rsa::Padding};
+use openssl::{hash::MessageDigest, rsa::Padding, sign::Verifier};
+use rsa::sha2::Sha256;
+use rsa::signature::SignatureEncoding;
+use rsa::signature::Signer;
+use rsa::{pkcs1v15::SigningKey, pkcs8::DecodePrivateKey, RsaPrivateKey};
 pub use rsa_sha256::RsaSha256;
-pub use hs2019::Hs2019;
 
 /// Find signature algorithm implementation by name
 pub fn by_name(name: &str) -> Option<Box<dyn Algorithm>> {
     match name {
         "rsa-sha256" => Some(Box::new(rsa_sha256::RsaSha256)),
-        "hs2019" => Some(Box::new(hs2019::Hs2019)),
         _ => None,
     }
 }
@@ -34,30 +34,20 @@ pub trait Algorithm {
     /// Sign data
     fn sign(&self, private_key: &PrivateKey, data: &[u8]) -> Result<Vec<u8>, Error> {
         let pkey = &private_key.0;
-        let mut signer = match self.message_digest() {
-            Some(message_digest) =>
-                Signer::new(message_digest, &pkey)?,
-            None =>
-                Signer::new_without_digest(&pkey)?,
-        };
-        if let Some(padding) = self.rsa_padding() {
-            signer.set_rsa_padding(padding)?;
-        }
-        let mut len = signer.len()?;
-        let mut buf = vec![0; len];
-        len = signer.sign_oneshot(&mut buf, data)?;
-        buf.truncate(len);
-        Ok(buf)
+        let pem = pkey.private_key_to_pem_pkcs8().unwrap();
+        let priv_key = RsaPrivateKey::from_pkcs8_pem(std::str::from_utf8(&pem).unwrap()).unwrap();
+        let signing_key = SigningKey::<Sha256>::new(priv_key);
+        let d = signing_key.sign(data);
+
+        Ok(d.to_vec())
     }
 
     /// Verify a signature
     fn verify(&self, public_key: &PublicKey, data: &[u8], signature: &[u8]) -> Result<bool, Error> {
         let pkey = &public_key.0;
         let mut verifier = match self.message_digest() {
-            Some(message_digest) =>
-                Verifier::new(message_digest, &pkey)?,
-            None =>
-                Verifier::new_without_digest(&pkey)?,
+            Some(message_digest) => Verifier::new(message_digest, &pkey)?,
+            None => Verifier::new_without_digest(&pkey)?,
         };
         if let Some(padding) = self.rsa_padding() {
             verifier.set_rsa_padding(padding)?;
